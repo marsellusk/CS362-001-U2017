48176173e226a19c1ebbb8b3d81e153a4dbe2d8d
marsellusk <marsellusk2@gmail.com>
2017-07-24 21:07:08 -0700

    Add files via upload

---
+++ src/test/java/calendar/CalDayTest.java
@@ -0,0 +1,199 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  CalDay class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalDayTest {
+
+	 @Test
+	  public void test01()  throws Throwable  { //testing variables with an appointment on one day
+		 
+		 GregorianCalendar test = new GregorianCalendar(2018,7,20);
+		 CalDay cal = new CalDay(test);
+		 
+		 int startHour=12;
+		 int startMinute=25;
+		 int startDay=20;
+		 int startMonth=7;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+
+		 cal.addAppt(appt);
+		 
+		 assertTrue(cal.isValid());
+		 assertNotNull(cal.iterator());
+		 assertNotNull(cal.getSizeAppts());
+		 assertEquals(20, cal.getDay());
+		 assertEquals(7, cal.getMonth());
+		 assertEquals(2018, cal.getYear());
+	 }
+	 
+	 @Test
+	  public void test02()  throws Throwable  { //testing variables with an in valid appointment 
+		 
+		 GregorianCalendar test = new GregorianCalendar(2018,7,20);
+		 CalDay cal = new CalDay(test);
+		 
+		 int startHour=12;
+		 int startMinute=-100;
+		 int startDay=20;
+		 int startMonth=7;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+
+		 cal.addAppt(appt);
+		 
+		 assertTrue(cal.isValid());
+		
+	 }
+	 
+ public void test03()  throws Throwable  { //testing variables with an appointment that is invalid in every way
+		 
+	 CalDay cal = new CalDay();
+	 
+	 assertFalse(cal.isValid());
+	 
+	 GregorianCalendar test = new GregorianCalendar(2018,7,20);
+	 cal = new CalDay(test);
+	 
+	 //invalid appt
+	 int startHour=120;
+	 int startMinute=250;
+	 int startDay=200;
+	 int startMonth=90;
+	 int startYear=2018;
+	 String title="Birthday Party";
+	 String description="This is my birthday party.";
+	 //Construct a new Appointment object with the initial data	 
+	 Appt appt = new Appt(startHour,
+	          startMinute ,
+	          startDay ,
+	          startMonth ,
+	          startYear ,
+	          title,
+	         description);
+
+	 cal.addAppt(appt);
+	 assertNull(cal.getAppts());
+		
+	 }
+	 
+	 @Test
+	  public void test04()  throws Throwable  { //add 3 appointments see if it is valid
+		 CalDay cal = new CalDay();
+		 
+		 assertFalse(cal.isValid());
+		 
+		 GregorianCalendar test = new GregorianCalendar(2018,7,20);
+		 cal = new CalDay(test);
+		 
+		 //invalid appt
+		 int startHour=120;
+		 int startMinute=250;
+		 int startDay=200;
+		 int startMonth=90;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+	//	 Appt appt = new Appt(startHour,
+	//	          startMinute ,
+	//	          startDay ,
+	//	          startMonth ,
+	//	          startYear ,
+	//	          title,
+	//	         description);
+
+	//	 cal.addAppt(appt);
+	//	 assertNull(cal.getAppts());
+		 
+		 
+		  startHour=18;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=7;
+		  startYear=2018;
+		  title="Birthday Dinner";
+		  description="This is my birthday Dinner.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+
+		 cal.addAppt(appt1);
+		 
+		 
+		 
+		  startHour=20;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=7;
+		  startYear=2018;
+		  title="Birthday Party";
+		  description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt2 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+
+		 cal.addAppt(appt2);
+		 
+		 
+		  startHour=9;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=7;
+		  startYear=2018;
+		  title="Birthday breakfast";
+		  description="This is my birthday breakfast.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt3 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+
+		 cal.addAppt(appt3);
+		 
+		 assertTrue(cal.isValid());
+		 
+		 cal.toString();
+	 }
+//add more unit tests as you needed	
+}
---
+++ src/test/java/calendar/TimeTableTest.java
@@ -0,0 +1,210 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  TimeTable class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTableTest {
+
+	 @Test
+	  public void test01()  throws Throwable  {
+		
+		 LinkedList<Appt> appts= new LinkedList<Appt>();	 
+		 
+		 int[] array1= {5};
+		 
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=20;
+		 int startMonth=7;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt1.setRecurrence(array1, appt1.RECUR_BY_YEARLY, 2, appt1.RECUR_NUMBER_FOREVER);
+		 appts.add(appt1);
+		 
+		  startHour=12;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=3;
+		  startYear=2018;
+		  title="Dad's Birthday Party";
+		  description="This is my Dad's birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt2 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt2.setRecurrence(array1, appt2.RECUR_BY_YEARLY, 2, appt2.RECUR_NUMBER_FOREVER);
+		 appts.add(appt2);
+		 
+		  startHour=12;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=10;
+		  startYear=2018;
+		  title="Friend's Birthday Party";
+		  description="This is my friend's birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt3 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt3.setRecurrence(array1, appt3.RECUR_BY_YEARLY, 2, appt3.RECUR_NUMBER_FOREVER);
+		 appts.add(appt3);
+		 
+		 GregorianCalendar day1 = new GregorianCalendar(2018,2,20);
+		 GregorianCalendar day2 = new GregorianCalendar(2018,11,20);
+		 
+		 TimeTable t1= new TimeTable();
+		 
+		 t1.getApptRange(appts,day1,day2);
+		 
+		 t1.deleteAppt(appts,appt3);
+		 
+		 int array2[] = {0,1};
+		 t1.permute(appts,array2);
+	 }
+	 
+	 @Test
+	  public void test02()  throws Throwable  {
+		 
+LinkedList<Appt> appts= new LinkedList<Appt>();	 
+		 
+		 int[] array1= {5};
+		 int[] array2= {};
+		 
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=20;
+		 int startMonth=7;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt1.setRecurrence(array2, appt1.RECUR_BY_WEEKLY, 2, appt1.RECUR_NUMBER_FOREVER);
+		 appts.add(appt1);
+		 
+		
+		 
+		 GregorianCalendar day1 = new GregorianCalendar(2018,2,20);
+		 GregorianCalendar day2 = new GregorianCalendar(2018,11,20);
+		 
+		 TimeTable t1= new TimeTable();
+		 
+		 t1.getApptRange(appts,day1,day2);
+		 
+		// appt1.setRecurrence(array2, appt1.RECUR_BY_WEEKLY, 2, appt1.RECUR_NUMBER_FOREVER); //length 0 array
+		
+	 }
+	 
+	 @Test
+	  public void test03()  throws Throwable  {
+		 
+ LinkedList<Appt> appts= new LinkedList<Appt>();	 
+		 
+		 int[] array1= {5};
+		 
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=20;
+		 int startMonth=7;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt1.setRecurrence(array1, appt1.RECUR_BY_YEARLY, 2, appt1.RECUR_NUMBER_FOREVER);
+		 appts.add(appt1);
+		 
+		  startHour=12;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=3;
+		  startYear=2018;
+		  title="Dad's Birthday Party";
+		  description="This is my Dad's birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt2 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt2.setRecurrence(array1, appt2.RECUR_BY_YEARLY, 2, appt2.RECUR_NUMBER_FOREVER);
+		 appts.add(appt2);
+		 
+		  startHour=12;
+		  startMinute=30;
+		  startDay=20;
+		  startMonth=10;
+		  startYear=2018;
+		  title="Friend's Birthday Party";
+		  description="This is my friend's birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt3 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 appt3.setRecurrence(array1, appt3.RECUR_BY_YEARLY, 2, appt3.RECUR_NUMBER_FOREVER);
+		 appts.add(appt3);
+		 
+		 GregorianCalendar day1 = new GregorianCalendar(2018,2,20);
+		 GregorianCalendar day2 = new GregorianCalendar(2018,11,20);
+		 
+		 TimeTable t1= new TimeTable();
+		 
+		 t1.getApptRange(appts,day1,day2);
+		 
+		 LinkedList<Appt> appts2= new LinkedList<Appt>(); //quickly testing for null exceptions
+		 appts2 = null;
+		 t1.deleteAppt(appts2,appt3);
+		 appt3 = null;
+		 t1.deleteAppt(appts,appt3);
+		 
+		 int array2[] = {0,1};
+		 t1.permute(appts,array2);
+		 
+	 }
+//add more unit tests as you needed
+}
